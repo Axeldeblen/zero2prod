764aed337acfc079620a53578958f64f0e6a2c7d
Axel Debenham-Lendon <axel@Axels-MacBook-Pro.local>
2024-05-04 13:01:40 +1000

    pg 38

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
+use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+use std::net::TcpListener;
+pub fn run(listner: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .listen(listner)?
+    .run();
+    Ok(server)
 }
 
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,13 @@
+use std::net::TcpListener;
 #[tokio::test]
 async fn health_check() {
-    spawn_app().await.expect("Failed to spawn our app.");
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
+    print!("{}", address);
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -13,6 +15,10 @@ async fn health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> Result<(), std::io::Error> {
-    //
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind to address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
